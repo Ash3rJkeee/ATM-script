d6eab0aafbdade9a5457b701b651169973d77c93
a.ryadinskih <a.ryadinskikh@gmail.com>
2020-10-02 23:55:28 +0300

    Прописаны все объекты котельных.

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ class Branch:
 
         class Pipe:
             """Класс описания вывода источника"""
-            def __init__(self, t1, t2, p1, p2, g, gp=None):
+            def __init__(self,  t1, t2, p1, p2, g, ta=None, gp=None):
+                self.ta = ta
                 self.t1 = t1
                 self.t2 = t2
                 self.p1 = p1
@@ -128,6 +129,7 @@ zf.rts_3 = zf.HeatSource(
 
 # Зеленоградский филиал РТС-4
 zf.rts_4 = zf.HeatSource(ta=find_param_by_id(dumps=dumps, id=2293106))
+
 zf.rts_4.pipe_1 = zf.rts_4.Pipe(
     t1=find_param_by_id(dumps=dumps, id=2293172),
     t2=find_param_by_id(dumps=dumps, id=2293174),
@@ -152,6 +154,7 @@ zf.rts_4.pipe_3 = zf.rts_4.Pipe(
     g=find_param_by_id(dumps=dumps, id=2293964),
 )
 
+# ХФ РТС-240
 hf.rts_240 = hf.HeatSource(
     ta=find_param_by_id(dumps=dumps, id=2312494),
     t1=find_param_by_id(dumps=dumps, id=2312568),
@@ -160,3 +163,147 @@ hf.rts_240 = hf.HeatSource(
     p2=find_param_by_id(dumps=dumps, id=2312578),
     g=find_param_by_id(dumps=dumps, id=2312546),
 )
+
+# ХФ КТС-Лавочкина
+hf.kts_lavochkina = hf.HeatSource(
+    ta=find_param_by_id(dumps=dumps, id=1112949),
+    t1=find_param_by_id(dumps=dumps, id=1113025),
+    t2=find_param_by_id(dumps=dumps, id=1113040),
+    p1=find_param_by_id(dumps=dumps, id=1113026),
+    p2=find_param_by_id(dumps=dumps, id=1113041),
+    g=find_param_by_id(dumps=dumps, id=1113028),
+    gp=find_param_by_id(dumps=dumps, id=1845173),
+)
+
+# ХФ КТС-Кольцевая
+hf.kts_kolcevaya = hf.HeatSource(
+    ta=find_param_by_id(dumps=dumps, id=1115461),
+    t1=find_param_by_id(dumps=dumps, id=1115537),
+    t2=find_param_by_id(dumps=dumps, id=1115552),
+    p1=find_param_by_id(dumps=dumps, id=1115538),
+    p2=find_param_by_id(dumps=dumps, id=1115553),
+    g=find_param_by_id(dumps=dumps, id=1115540),
+    gp=find_param_by_id(dumps=dumps, id=1840894),
+)
+
+# ХФ КТС-Мичурина
+hf.kts_michurina = hf.HeatSource(
+    ta=find_param_by_id(dumps=dumps, id=1112351),
+    t1=find_param_by_id(dumps=dumps, id=1112313),
+    t2=find_param_by_id(dumps=dumps, id=1112314),
+    p1=find_param_by_id(dumps=dumps, id=1112317),
+    p2=find_param_by_id(dumps=dumps, id=1112318),
+    g=find_param_by_id(dumps=dumps, id=1112319)
+)
+
+# ХФ КТС-Октябрьская
+hf.kts_oktyabrskaya = hf.HeatSource(
+    ta=find_param_by_id(dumps=dumps, id=2129258),
+    t1=find_param_by_id(dumps=dumps, id=2129272),
+    t2=find_param_by_id(dumps=dumps, id=2129273),
+    p1=find_param_by_id(dumps=dumps, id=2129270),
+    p2=find_param_by_id(dumps=dumps, id=2129271),
+    g=find_param_by_id(dumps=dumps, id=2129269)
+)
+
+# ХФ КТС-Банный
+hf.kts_bannyi = hf.HeatSource(
+    ta=find_param_by_id(dumps=dumps, id=1859393),
+    t1=find_param_by_id(dumps=dumps, id=1859469),
+    t2=find_param_by_id(dumps=dumps, id=1859484),
+    p1=find_param_by_id(dumps=dumps, id=1859470),
+    p2=find_param_by_id(dumps=dumps, id=1859485),
+    g=find_param_by_id(dumps=dumps, id=1859472)
+)
+
+# ХФ КТС-Мира
+hf.kts_mira = hf.HeatSource(
+    ta=find_param_by_id(dumps=dumps, id=1005770),
+    t1=find_param_by_id(dumps=dumps, id=1005775),
+    t2=find_param_by_id(dumps=dumps, id=1005776),
+    p1=find_param_by_id(dumps=dumps, id=1005782),
+    p2=find_param_by_id(dumps=dumps, id=1005783),
+    g=find_param_by_id(dumps=dumps, id=1005772)
+)
+
+# ХФ КТС-Фрунзе
+hf.kts_frunze = hf.HeatSource(
+    ta=find_param_by_id(dumps=dumps, id=2314872)
+)
+
+# ХФ КТС-Речная
+hf.kts_rechnaya = hf.HeatSource(
+    ta=find_param_by_id(dumps=dumps, id=1864136),
+    t1=find_param_by_id(dumps=dumps, id=1864141),
+    t2=find_param_by_id(dumps=dumps, id=1864142),
+    p1=find_param_by_id(dumps=dumps, id=1864148),
+    p2=find_param_by_id(dumps=dumps, id=1864149),
+    g=find_param_by_id(dumps=dumps, id=1864138)
+)
+
+# Электрогорский фиилал
+ef.gres_3 = ef.HeatSource()
+
+ef.gres_3.el_nip = ef.gres_3.Pipe(
+    t1=find_param_by_id(dumps=dumps, id=2244106),
+    t2=find_param_by_id(dumps=dumps, id=2244108),
+    p1=find_param_by_id(dumps=dumps, id=2244086),
+    p2=find_param_by_id(dumps=dumps, id=2244090),
+    g=find_param_by_id(dumps=dumps, id=2244046),
+)
+
+ef.gres_3.el_nip = ef.gres_3.Pipe(
+    ta=find_param_by_id(dumps=dumps, id=2244010),
+    t1=find_param_by_id(dumps=dumps, id=2244106),
+    t2=find_param_by_id(dumps=dumps, id=2244108),
+    p1=find_param_by_id(dumps=dumps, id=2244086),
+    p2=find_param_by_id(dumps=dumps, id=2244090),
+    g=find_param_by_id(dumps=dumps, id=2244046),
+)
+
+ef.gres_3.g_1 = ef.gres_3.Pipe(
+    ta=find_param_by_id(dumps=dumps, id=2242826),
+    t1=find_param_by_id(dumps=dumps, id=2242908),
+    t2=find_param_by_id(dumps=dumps, id=2242910),
+    p1=find_param_by_id(dumps=dumps, id=2242888),
+    p2=find_param_by_id(dumps=dumps, id=2242892),
+    g=find_param_by_id(dumps=dumps, id=2242862),
+)
+
+ef.gres_3.g_2 = ef.gres_3.Pipe(
+    ta=find_param_by_id(dumps=dumps, id=2243522),
+    t1=find_param_by_id(dumps=dumps, id=2243618),
+    t2=find_param_by_id(dumps=dumps, id=2243620),
+    p1=find_param_by_id(dumps=dumps, id=2243598),
+    p2=find_param_by_id(dumps=dumps, id=2243602),
+    g=find_param_by_id(dumps=dumps, id=2243558),
+)
+
+ef.gres_3.l_1 = ef.gres_3.Pipe(
+    ta=find_param_by_id(dumps=dumps, id=2228460),
+    t1=find_param_by_id(dumps=dumps, id=2228556),
+    t2=find_param_by_id(dumps=dumps, id=2228558),
+    p1=find_param_by_id(dumps=dumps, id=2228536),
+    p2=find_param_by_id(dumps=dumps, id=2228540),
+    g=find_param_by_id(dumps=dumps, id=2228496),
+)
+
+ef.gres_3.s_1 = ef.gres_3.Pipe(
+    ta=find_param_by_id(dumps=dumps, id=2243766),
+    t1=find_param_by_id(dumps=dumps, id=2243862),
+    t2=find_param_by_id(dumps=dumps, id=2243864),
+    p1=find_param_by_id(dumps=dumps, id=2243842),
+    p2=find_param_by_id(dumps=dumps, id=2243846),
+    g=find_param_by_id(dumps=dumps, id=2243802),
+)
+
+# ПФ КТС-Отрадное
+pf.kts_otradnoe = pf.HeatSource(
+    ta=find_param_by_id(dumps=dumps, id=2305740),
+    t1=find_param_by_id(dumps=dumps, id=2305806),
+    t2=find_param_by_id(dumps=dumps, id=2305808),
+    p1=find_param_by_id(dumps=dumps, id=2305816),
+    p2=find_param_by_id(dumps=dumps, id=2305818),
+    g=find_param_by_id(dumps=dumps, id=2305794),
+)
+
